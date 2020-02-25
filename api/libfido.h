00357ac844701324be82143d75b4c08e445d8a4f
Philippe Thierry <phil@reseau-libre.net>
2020-02-25 21:33:09 +0100

    [init] initial commit, basics

---
+++ api/libfido.h
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright 2019 The wookey project team <wookey@ssi.gouv.fr>
+ *   - Ryad     Benadjila
+ *   - Arnauld  Michelizza
+ *   - Mathieu  Renard
+ *   - Philippe Thierry
+ *   - Philippe Trebuchet
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * ur option) any later version.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this package; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+#ifndef LIBFIDO_H_
+#define LIBFIDO_H_
+
+#include "autoconf.h"
+#include "libc/types.h"
+
+mbed_error_t fido_declare(void);
+
+mbed_error_t fido_configure(void);
+
+#endif/*!LIBFIDO_H_*/
